dade23d7be2902f38f425633ca02e4100ac681dd
katya <katyamatya21@gmail.com>
2015-11-11 22:17:12 +0300

    module1-task2

---
+++ js/check.js
@@ -0,0 +1,52 @@
+function getMessage(a, b) {
+
+    if (typeof(a) == "boolean") {
+
+        if (a == true) {
+
+            return 'Я попал в ' + b;
+
+        } else {
+
+            return 'Я никуда не попал';
+        }
+    }
+
+    if (typeof(a) == "number") {
+
+        return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+
+    }
+
+    if ( Array.isArray(a) && !Array.isArray(b)) {
+
+        var sum = 0;
+        var size = a.length;
+
+
+        for (var i = 0; i < size; i++) {
+
+            sum = sum + a[i];
+        }
+
+        return 'Я прошел ' + sum + ' шагов';
+    }
+
+
+    if ( Array.isArray(a) && Array.isArray(b)) {
+
+        var length = 0;
+        var summa = 0;
+        var size = a.length;
+
+        for (var i = 0; i < size; i++) {
+
+            summa = a[i] * b[i];
+            length = length + summa;
+        }
+
+        return 'Я прошел ' + length + ' шагов';
+
+    }
+
+}
