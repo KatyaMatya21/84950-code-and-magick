a209a8c81c9cb39972e68482c5e732c49060b62f
katya <katyamatya21@gmail.com>
2015-11-20 20:47:21 +0300

    Исправления module3-task1

--- js/form.js
+++ js/form.js
@@ -20,48 +20,48 @@
   var userReview = document.querySelector('.review-form-field-text');
   var sendReview = document.querySelector('.review-submit');
 
-  function formValid(){
+  function formValid() {
     sendReview.disabled = true;
-    document.querySelector('.review-fields').style.display='inline-block';
+    document.querySelector('.review-fields').style.display = 'inline-block';
     var userMarkActive = document.querySelector('input[name="review-mark"]:checked');
-    if(parseInt(userMarkActive.value) < 3){
+    if (parseInt(userMarkActive.value, 10) < 3) {
       userReview.required = true;
-      if(userReview.value.length < 1) {
+      if ( userReview.value.length < 1 ) {
         return true;
       }
-    }else{
+    }else {
       userReview.required = false;
     }
-    if(userName.value.length < 1) {
+    if (userName.value.length < 1) {
       return true;
     }
     sendReview.disabled = false;
-    document.querySelector('.review-fields').style.display='none';
+    document.querySelector('.review-fields').style.display = 'none';
   }
 
-  for(var i = 0; i < userMark.length; i++ ){
+  for (var i = 0; i < userMark.length; i++ ) {
     userMark[i].onclick = formValid;
   }
-  userName.onkeyup = function(){
-    if(userName.value.length < 1) {
-      document.querySelector('.review-fields-name').style.display='inline-block';
-    }else{
-      document.querySelector('.review-fields-name').style.display='none';
+  userName.onkeyup = function() {
+    if (userName.value.length < 1) {
+      document.querySelector('.review-fields-name').style.display = 'inline-block';
+    }else {
+      document.querySelector('.review-fields-name').style.display = 'none';
     }
 
     formValid();
 
   };
-  userReview.onkeyup = function(){
+  userReview.onkeyup = function() {
     formValid();
-    if(userReview.required == true){
-      if(userReview.value.length < 1) {
-        document.querySelector('.review-fields-text').style.display='inline-block';
-      }else{
-        document.querySelector('.review-fields-text').style.display='none';
+    if (userReview.required === true) {
+      if (userReview.value.length < 1) {
+        document.querySelector('.review-fields-text').style.display = 'inline-block';
+      }else {
+        document.querySelector('.review-fields-text').style.display = 'none';
       }
-    }else{
-      document.querySelector('.review-fields-text').style.display='none';
+    }else {
+      document.querySelector('.review-fields-text').style.display = 'none';
     }
   };
 })();
