3c3b91e70e9bc95ef64842e12c751751051f6e55
katya <katyamatya21@gmail.com>
2015-12-14 23:58:20 +0300

    Дополнение module8-task2 jsdoc

--- js/inherit.js
+++ js/inherit.js
@@ -1,6 +1,11 @@
 
 'use strict';
 
+/**
+ * Наследование
+ * @param {Object} child
+ * @param {Object} parent
+ */
 function inherit(child, parent) {
   var EmptyConstructor = function() {};
   EmptyConstructor.prototype = parent.prototype;
